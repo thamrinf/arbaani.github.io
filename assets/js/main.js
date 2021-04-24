fdcf54681cfed26b7ebe2ee3b424b5b75fc68bab
arbaani <73212630+arbaani@users.noreply.github.com>
2021-04-24 15:34:42 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,3 +58,9 @@ var typed = new Typed(".typing", {
 /*SCROLL DESIGN*/
     sr.reveal('.design__img', { interval: 200 });
  
+/*PRELOADING*/ 
+    window.onload = () => {
+        setTimeout(() => {
+          document.querySelector("body").classList.add("display");
+        }, 4000);
+      };
